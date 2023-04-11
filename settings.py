ff9d82e2045480992a624b1e4bb1632f4de5ee78
hiksalmuhammad <90540819+hiksalmuhammad@users.noreply.github.com>
2023-04-11 17:42:48 +0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A Class to store all settings for Alien Invasion."""
+    
+    def __init__(self):
+        """Initialize the game's static settings."""
+        #Screen settings
+        self.screen_width = 960
+        self.screen_height = 720
+        self.bg_color = (230, 230, 230)
